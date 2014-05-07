4108481436c74ab0ce1e5f7690dbfbb469492a28
Ryan Campbell <ryan.campbell@gmail.com>
2014-05-07 14:14:24 -0500

    [JENKINS-19587] Support uploading mulitple file entries
    Use standard Jenkins form display and processing facilities to get dynamic select box population and reduce overall code.

--- src/main/java/hudson/plugins/s3/Entry.java
+++ src/main/java/hudson/plugins/s3/Entry.java
@@ -1,8 +1,13 @@
 package hudson.plugins.s3;
 
 import com.amazonaws.regions.Regions;
+import hudson.Extension;
+import hudson.model.AbstractDescribableImpl;
+import hudson.model.Descriptor;
+import hudson.util.ListBoxModel;
+import org.kohsuke.stapler.DataBoundConstructor;
 
-public final class Entry {
+public final class Entry  extends AbstractDescribableImpl <Entry> {
     /**
      * Destination bucket for the copy. Can contain macros.
      */
@@ -43,4 +48,38 @@ public final class Entry {
      * Let Jenkins manage the S3 uploaded artifacts
      */
     public boolean managedArtifacts;
+
+    @DataBoundConstructor
+    public Entry(String bucket, String sourceFile, String storageClass, String selectedRegion, boolean noUploadOnFailure,
+                 boolean uploadFromSlave, boolean managedArtifacts) {
+        this.bucket = bucket;
+        this.sourceFile = sourceFile;
+        this.storageClass = storageClass;
+        this.selectedRegion = selectedRegion;
+        this.noUploadOnFailure = noUploadOnFailure;
+        this.uploadFromSlave = uploadFromSlave;
+        this.managedArtifacts = managedArtifacts;
+    }
+
+    @Extension
+    public static class DescriptorImpl extends Descriptor<Entry> {
+        public String getDisplayName() { return "S3 Upload File Entry"; }
+
+        public ListBoxModel doFillStorageClassItems() {
+            ListBoxModel items = new ListBoxModel();
+            for (String storageClass : Entry.storageClasses) {
+                items.add(storageClass);
+            }
+            return items;
+        }
+
+        public ListBoxModel doFillSelectedRegionItems() {
+            ListBoxModel items = new ListBoxModel();
+            for (Regions region : Entry.regions) {
+                items.add(region.getName());
+            }
+            return items;
+        }
+
+    }
 }
